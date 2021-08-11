6c41dbf4e04436450320895275dc7d3a8b4916c1
selfim <lm233427@gmail.com>
2021-08-11 16:13:20 +0800

    增加索引信息

--- src/DataDict.php
+++ src/DataDict.php
@@ -15,6 +15,10 @@ class DataDict
         $this->db = new PDO('mysql:dbname='.$config['dbname'].';host='.$config['host'], $config['user'], $config['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         $this->db->query('SET NAMES utf8');
     }
+    public static function init(array $config): DataDict
+    {
+        return new self($config);
+    }
     public  function make(string $type = 'markdown')
     {
         switch ($type) {
@@ -22,17 +26,25 @@ class DataDict
                 return $this->generateHtml();
             case 'json':
                 return $this->generateJson();
+            case 'doc':
+                return $this->generateDoc();
+            case 'xls':
+                return $this->generateXls();
             default:
                 return $this->generateMd();
         }
     }
+
+    public function common()
+    {
+
+    }
+
     protected  function getTables()
     {
         $list = $tmp = [];
         $res = $this->db->query("SHOW TABLE STATUS");
-        //echo'<pre>';var_export($res);die();
         while ( $row =  $res->fetch(PDO::FETCH_ASSOC)) {
-            //$tables [] ['TABLE_NAME'] = current($row);
             $tmp['name']    = $row['Name'];
             $tmp['comment'] = $row['Comment'];
             $tmp['engine']  = $row['Engine'];
@@ -61,11 +73,21 @@ class DataDict
         unset($tmp);
         return $list;
     }
+    protected  function getIndexInfo($arrIndexInfo)
+    {
+        $index = [];
+        foreach ($arrIndexInfo as $v) {
+            $unique = ($v['Non_unique'] == 0) ? '' : '(unique)';
+            // $index[$v['Key_name']][] = $v['Seq_in_index'].': '.$v['Column_name'].$unique;
+            $index[$v['Key_name']][] = $v['Column_name'].$unique;
+        }
+
+        return $index;
+    }
     protected  function generateHtml()
     {
-        $title = $this->database.'数据库'. '数据字典';
+        $title = $this->database. '数据字典';
         $tables   = $this->getTables();
-
         foreach ( $tables as $k => $v ) {
             $sql = 'SELECT * FROM ';
             $sql .= 'INFORMATION_SCHEMA.TABLES ';
@@ -87,10 +109,111 @@ class DataDict
                 $fields [] = $t;
             }
             $tables [$k] ['COLUMN'] = $fields;
+
+            //索引
+            $index = [];
+            $sql = 'SHOW INDEX FROM '."{$v['name']}";
+            $res = $this->db->query ( $sql );
+            while ( $i = $res->fetch(PDO::FETCH_ASSOC) ) {
+
+                $index[] = $i;
+            }
+            $tables [$k] ['INDEX'] = $this->getIndexInfo($index);
+        }
+
+        $html = '';
+        // 循环所有表
+        foreach ( $tables as $k => $v ) {
+            // $html .= '<p><h2>'. $v['TABLE_COMMENT'] . ' </h2>';
+            $html .= '<table  border="1" cellspacing="0" cellpadding="0" align="center">';
+            $html .= '<caption>' . $v ['name'] . '  ' . $v ['TABLE_COMMENT'] . '</caption>';
+            $html .= '<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th>
+                        <th>允许非空</th>
+            <th>自动递增</th><th>备注(字段数: '. count($v['COLUMN']).')</th></tr>';
+            $html .= '';
+
+            foreach ( $v ['COLUMN'] as $f ) {
+                $html .= '<tr><td class="c1">' . $f ['COLUMN_NAME'] . '</td>';
+                $html .= '<td class="c2">' . $f ['COLUMN_TYPE'] . '</td>';
+                $html .= '<td class="c3"> ' . $f ['COLUMN_DEFAULT'] . '</td>';
+                $html .= '<td class="c4"> ' . $f ['IS_NULLABLE'] . '</td>';
+                $html .= '<td class="c5">' . ($f ['EXTRA'] == 'auto_increment' ? '是' : ' ') . '</td>';
+                $html .= '<td class="c6"> ' . $f ['COLUMN_COMMENT'] . '</td>';
+                $html .= '</tr>';
+            }
+            if (!empty($v['INDEX'])) {
+                $html .= '<tr><th colspan="2">索引名</th><th colspan="4">索引顺序</th></tr>';
+                foreach ($v['INDEX'] as $indexName => $indexContent) {
+                    $html .= '<tr>'.PHP_EOL;
+                    $html .= '<td class="c7" colspan="2">' . $indexName . '</td>'.PHP_EOL;
+                    $html .= '<td class="c8" colspan="4">' . implode(' > ', $indexContent) . '</td>'.PHP_EOL;
+                    $html .= '</tr>'.PHP_EOL;
+                }
+            }
+            $html .= '</tbody></table></p>';
+        }
+        $header = '<html>
+                <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>' . $title . '</title>
+                <style>
+                body,td,th {font-family:"宋体"; font-size:12px;}
+                table{border-collapse:collapse;border:1px solid #CCC;background:#6089D4;}
+                table caption{text-align:left; background-color:#fff; line-height:2em; font-size:14px; font-weight:bold; }
+                table th{text-align:left; font-weight:bold;height:26px; line-height:25px; font-size:16px; border:3px solid #fff; color:#ffffff; padding:5px;}
+                table td{height:25px; font-size:12px; border:3px solid #fff; background-color:#f0f0f0; padding:5px;}
+                .c1{ width: 150px;}
+                .c2{ width: 130px;}
+                .c3{ width: 70px;}
+                .c4{ width: 80px;}
+                .c5{ width: 80px;}
+                .c6{ width: 300px;}
+                .c7{ width: 200px;}
+                .c8{ width: 515px;}
+                </style>
+                </head>
+                <body>';
+        return $header . '<h1 style="text-align:center;">' . $title . '</h1>'  .
+            '<header><p style="text-align:center;margin:20px auto;">总共：' . count($tables) .'个数据表'."&nbsp".'生成时间：' . date('Y-m-d H:i:s'). '</p></header>'
+            .$html. '</body></html>';
+
+    }
+    protected function generateDoc()
+    {
+        header("Content-type:text/html;charset=utf-8");
+        header("Content-type: application/octet-stream");
+        header("Accept-Ranges: bytes");
+        header("Content-type:application/vnd.ms-word");
+        header('Cache-Control: max-age=0');
+        header("Content-Disposition:attachment;filename={$this->database}数据字典.docx");
+        $title = $this->database. '数据字典';
+        $tables = $this->getTables();
+
+        foreach ($tables as $k => $v) {
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.TABLES ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}'  AND table_schema = '{$this->database}'";
+            $res = $this->db->query($sql);
+            while ($t = $res->fetch(PDO::FETCH_ASSOC)) {
+                $tables [$k] ['TABLE_COMMENT'] = $t ['TABLE_COMMENT'];
+            }
+
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}' AND table_schema = '{$this->database}'";
+
+            $fields = [];
+            $res = $this->db->query($sql);
+            while ($t = $res->fetch(PDO::FETCH_ASSOC)) {
+                $fields [] = $t;
+            }
+            $tables [$k] ['COLUMN'] = $fields;
         }
         $html = '';
-// 循环所有表
-        foreach ( $tables as $k => $v ) {
+        // 循环所有表
+        foreach ($tables as $k => $v) {
             // $html .= '<p><h2>'. $v['TABLE_COMMENT'] . ' </h2>';
             $html .= '<table  border="1" cellspacing="0" cellpadding="0" align="center">';
             $html .= '<caption>' . $v ['name'] . '  ' . $v ['TABLE_COMMENT'] . '</caption>';
@@ -99,7 +222,7 @@ class DataDict
     <th>自动递增</th><th>备注</th></tr>';
             $html .= '';
 
-            foreach ( $v ['COLUMN'] as $f ) {
+            foreach ($v ['COLUMN'] as $f) {
                 $html .= '<tr><td class="c1">' . $f ['COLUMN_NAME'] . '</td>';
                 $html .= '<td class="c2">' . $f ['COLUMN_TYPE'] . '</td>';
                 $html .= '<td class="c3"> ' . $f ['COLUMN_DEFAULT'] . '</td>';
@@ -129,10 +252,90 @@ table td{height:25px; font-size:12px; border:3px solid #fff; background-color:#f
 </style>
 </head>
 <body>';
-        return  $header.'<h1 style="text-align:center;">' . $title . '</h1>'.$html.
-            '<p style="text-align:left;margin:20px auto;">总共：' . count($tables) . '个数据表</p>'.
+        $data = $header . '<h1 style="text-align:center;">' . $title . '</h1>'  . '<p style="text-align:center;margin:20px auto;">生成时间：' . date('Y-m-d H:i:s') . '</p>'.
+            '<p style="text-align:left;margin:20px auto;">总共：' . count($tables) . '个数据表</p>' . '<p style="text-align:center;margin:20px auto;">生成时间：' . date('Y-m-d H:i:s') .$html.
             '</body></html>';
+        echo $data;
+    }
+    protected function generateXls()
+    {
+//        header("Content-type:text/csv");
+//        header("Content-Disposition:attachment;filename={$this->database}数据字典.csv");
+//        header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
+//        header('Expires:0');
+//        header('Pragma:public');
+        header("Content-type: application/vnd.ms-excel; charset=gbk");
+        header("Content-Disposition: attachment; filename={$this->database}数据字典.xls");
+        $title = $this->database. '数据字典';
+        $tables = $this->getTables();
 
+        foreach ($tables as $k => $v) {
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.TABLES ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}'  AND table_schema = '{$this->database}'";
+            $res = $this->db->query($sql);
+            while ($t = $res->fetch(PDO::FETCH_ASSOC)) {
+                $tables [$k] ['TABLE_COMMENT'] = $t ['TABLE_COMMENT'];
+            }
+
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}' AND table_schema = '{$this->database}'";
+
+            $fields = [];
+            $res = $this->db->query($sql);
+            while ($t = $res->fetch(PDO::FETCH_ASSOC)) {
+                $fields [] = $t;
+            }
+            $tables [$k] ['COLUMN'] = $fields;
+        }
+        $html = '';
+        // 循环所有表
+        foreach ($tables as $k => $v) {
+            // $html .= '<p><h2>'. $v['TABLE_COMMENT'] . ' </h2>';
+            $html .= '<table  border="1" cellspacing="0" cellpadding="0" align="center">';
+            $html .= '<caption>' . $v ['name'] . '  ' . $v ['TABLE_COMMENT'] . '</caption>';
+            $html .= '<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th>
+    <th>允许非空</th>
+    <th>自动递增</th><th>备注</th></tr>';
+            $html .= '';
+
+            foreach ($v ['COLUMN'] as $f) {
+                $html .= '<tr><td class="c1">' . $f ['COLUMN_NAME'] . '</td>';
+                $html .= '<td class="c2">' . $f ['COLUMN_TYPE'] . '</td>';
+                $html .= '<td class="c3"> ' . $f ['COLUMN_DEFAULT'] . '</td>';
+                $html .= '<td class="c4"> ' . $f ['IS_NULLABLE'] . '</td>';
+                $html .= '<td class="c5">' . ($f ['EXTRA'] == 'auto_increment' ? '是' : ' ') . '</td>';
+                $html .= '<td class="c6"> ' . $f ['COLUMN_COMMENT'] . '</td>';
+                $html .= '</tr>';
+            }
+            $html .= '</tbody></table></p>';
+        }
+        $header = '<html>
+                <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>' . $title . '</title>
+                <style>
+                body,td,th {font-family:"宋体"; font-size:12px;}
+                table{border-collapse:collapse;border:1px solid #CCC;background:#6089D4;}
+                table caption{text-align:left; background-color:#fff; line-height:2em; font-size:14px; font-weight:bold; }
+                table th{text-align:left; font-weight:bold;height:26px; line-height:25px; font-size:16px; border:3px solid #fff; color:#ffffff; padding:5px;}
+                table td{height:25px; font-size:12px; border:3px solid #fff; background-color:#f0f0f0; padding:5px;}
+                .c1{ width: 150px;}
+                .c2{ width: 130px;}
+                .c3{ width: 70px;}
+                .c4{ width: 80px;}
+                .c5{ width: 80px;}
+                .c6{ width: 300px;}
+                </style>
+                </head>
+                <body>';
+        $data = $header . '<h1 style="text-align:center;">' . $title . '</h1>'  . '<p style="text-align:center;margin:20px auto;">生成时间：' . date('Y-m-d H:i:s') . '</p>'.
+            '<p style="text-align:left;margin:20px auto;">总共：' . count($tables) . '个数据表</p>' . '<p style="text-align:center;margin:20px auto;">生成时间：' . date('Y-m-d H:i:s') .$html.
+            '</body></html>';
+        echo $data;
     }
     protected  function generateJson()
     {
