d05cc78ed5ffdddeb21d086c5a4b391db5f1c229
selfim <lm233427@gmail.com>
2021-08-11 08:38:28 +0800

    调整输出html错误

--- src/DataDict.php
+++ src/DataDict.php
@@ -8,10 +8,11 @@ use PDO;
 class DataDict
 {
     protected $db;
+    protected  $database;
     public function __construct(array $config)
     {
-        //'mysql:dbname='.$config['dbname'].';host='.$config['host']
-        $this->db = new PDO($config['dsn'], $config['user'], $config['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $this->database = $config['dbname'];
+        $this->db = new PDO('mysql:dbname='.$config['dbname'].';host='.$config['host'], $config['user'], $config['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         $this->db->query('SET NAMES utf8');
     }
     public  function make(string $type = 'markdown')
@@ -29,6 +30,7 @@ class DataDict
     {
         $list = $tmp = [];
         $res = $this->db->query("SHOW TABLE STATUS");
+        //echo'<pre>';var_export($res);die();
         while ( $row =  $res->fetch(PDO::FETCH_ASSOC)) {
             //$tables [] ['TABLE_NAME'] = current($row);
             $tmp['name']    = $row['Name'];
@@ -61,51 +63,74 @@ class DataDict
     }
     protected  function generateHtml()
     {
+        $title = $this->database.'数据库'. '数据字典';
         $tables   = $this->getTables();
-        $html     = '<!DOCTYPE html>
-<html lang="zh">
-<head>
-	<meta charset="UTF-8">
-	<title>数据字典</title>
-	<style>
-		body, td, th{font-family:"宋体"; font-size:14px;}
-		table, h1, p,.menu{width:960px;margin:0px auto;}
-		table{border-collapse:collapse;border:1px solid #CCC;background:#efefef;}
-		table caption{text-align:left; background-color:#fff; line-height:2em; font-size:14px; font-weight:bold; }
-		table th{text-align:left; font-weight:bold;height:26px; line-height:26px; font-size:12px; border:1px solid #CCC;padding-left:5px;}
-		table td{height:20px; font-size:12px; border:1px solid #CCC;background-color:#fff;padding-left:5px;}
-		a{text-decoration:none}
-		.w150{ width:150px;}
-		.w80{ width:80px;}
-		.w100{ width:100px;}
-		.w300{ width:300px;}
-	</style>
-</head>
-<body ><h1 style="text-align:center;">数据字典</h1>';
-        $menu     = '<div class="menu">';
-        $infoList = '';
-        foreach ($tables as $k => $val) {
-            $menu     .= '<a href="#' . $k . '">' . ($k + 1) . '……' . $val['name'] . '   ' . $val['comment'] . '</a> <br/>';
-            $info     = $this->generateHtml($val['name']);
-            $infoList .= '<table id="' . $k . '" border="1" cellspacing="0" cellpadding="0" align="center">';
-            $infoList .= '<caption>表名：' . $val['name'] . '<span style="font-size: 12px;color:#9e9e9e">【' . $val['engine'] . '】</span>' . $val['comment'] . '</caption>';
-            $infoList .= '<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th><th>允许非空</th><th>自动递增</th><th>备注</th></tr>';
-            $infoList .= '<br/>';
-            foreach ($info as $item) {
-                $infoList .= '<td class="w150">' . $item['field'] . '</td>';
-                $infoList .= '<td class="w150">' . $item['type'] . '</td>';
-                $infoList .= '<td class="w80">' . $item['default'] . '</td>';
-                $infoList .= '<td class="w100">' . $item['null'] . '</td>';
-                $infoList .= '<td class="w100">' . ($item['extra'] === 'auto_increment' ? '是' : ' ') . '</td>';
-                $infoList .= '<td class="w300">' . $item['comment'] . '</td>';
-                $infoList .= '</tr>';
-            }
-        }
 
-        $html .= $menu . '</div>' . $infoList;
-        $html .= '</tbody></table></p>';
-        $html .= '<p style="text-align:left;margin:20px auto;">总共：' . count($tables) . '个数据表</p></body></html>';
-        return $html;
+        foreach ( $tables as $k => $v ) {
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.TABLES ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}'  AND table_schema = '{$this->database}'";
+            $res = $this->db->query ( $sql );
+            while ( $t = $res->fetch(PDO::FETCH_ASSOC) ) {
+                $tables [$k] ['TABLE_COMMENT'] = $t ['TABLE_COMMENT'];
+            }
+
+            $sql = 'SELECT * FROM ';
+            $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
+            $sql .= 'WHERE ';
+            $sql .= "table_name = '{$v['name']}' AND table_schema = '{$this->database}'";
+
+            $fields = [];
+            $res = $this->db->query ( $sql );
+            while ( $t = $res->fetch(PDO::FETCH_ASSOC) ) {
+                $fields [] = $t;
+            }
+            $tables [$k] ['COLUMN'] = $fields;
+        }
+        $html = '';
+// 循环所有表
+        foreach ( $tables as $k => $v ) {
+            // $html .= '<p><h2>'. $v['TABLE_COMMENT'] . ' </h2>';
+            $html .= '<table  border="1" cellspacing="0" cellpadding="0" align="center">';
+            $html .= '<caption>' . $v ['name'] . '  ' . $v ['TABLE_COMMENT'] . '</caption>';
+            $html .= '<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th>
+    <th>允许非空</th>
+    <th>自动递增</th><th>备注</th></tr>';
+            $html .= '';
+
+            foreach ( $v ['COLUMN'] as $f ) {
+                $html .= '<tr><td class="c1">' . $f ['COLUMN_NAME'] . '</td>';
+                $html .= '<td class="c2">' . $f ['COLUMN_TYPE'] . '</td>';
+                $html .= '<td class="c3"> ' . $f ['COLUMN_DEFAULT'] . '</td>';
+                $html .= '<td class="c4"> ' . $f ['IS_NULLABLE'] . '</td>';
+                $html .= '<td class="c5">' . ($f ['EXTRA'] == 'auto_increment' ? '是' : ' ') . '</td>';
+                $html .= '<td class="c6"> ' . $f ['COLUMN_COMMENT'] . '</td>';
+                $html .= '</tr>';
+            }
+            $html .= '</tbody></table></p>';
+        }
+        $header = '<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>' . $title . '</title>
+<style>
+body,td,th {font-family:"宋体"; font-size:12px;}
+table{border-collapse:collapse;border:1px solid #CCC;background:#6089D4;}
+table caption{text-align:left; background-color:#fff; line-height:2em; font-size:14px; font-weight:bold; }
+table th{text-align:left; font-weight:bold;height:26px; line-height:25px; font-size:16px; border:3px solid #fff; color:#ffffff; padding:5px;}
+table td{height:25px; font-size:12px; border:3px solid #fff; background-color:#f0f0f0; padding:5px;}
+.c1{ width: 150px;}
+.c2{ width: 130px;}
+.c3{ width: 70px;}
+.c4{ width: 80px;}
+.c5{ width: 80px;}
+.c6{ width: 300px;}
+</style>
+</head>
+<body>';
+        return  $header.'<h1 style="text-align:center;">' . $title . '</h1>'.$html.'</body></html>';
+
     }
     protected  function generateJson()
     {
